1fccd640673dee9aa1fc58370aa9fc44a16441d0
sturniolo <br1.sturniolo@gmail.com>
2022-11-10 14:25:00 -0300

    [ADD] eurekaServer

---
+++ Eureka/src/test/java/com/dh/Eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
