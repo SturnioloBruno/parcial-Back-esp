a6a3755bd1c44179681e2c21ae27f45a0d0878aa
sturniolo <br1.sturniolo@gmail.com>
2022-12-12 15:05:50 -0300

    [ADD] ListenerMovies

--- api-movie/src/main/java/com/dh/movie/api/controller/MovieController.java
+++ api-movie/src/main/java/com/dh/movie/api/controller/MovieController.java
@@ -1,30 +1,40 @@
 package com.dh.movie.api.controller;
 
+import com.dh.movie.api.queue.MovieListener;
 import com.dh.movie.domain.model.Movie;
 import com.dh.movie.api.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RefreshScope
+@RequiredArgsConstructor
 @RequestMapping("/movies")
 public class MovieController {
 
 
     private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
-        this.movieService = movieService;
-    }
+    private final MovieListener movieListener;
+
 
     @GetMapping("/{genre}")
     ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
         return ResponseEntity.ok().body(movieService.findByGenre(genre));
     }
 
+    /*
     @PostMapping("/save")
     ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
         return ResponseEntity.ok().body(movieService.save(movie));
+    }*/
+
+    @PostMapping("/salvar")
+    public void saveMovie(@RequestBody Movie movie) {
+        movieListener.receive(movie);
     }
 }
