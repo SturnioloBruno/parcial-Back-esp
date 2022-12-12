1eda07cbfd1133fff8142b1cbbdddc0918182896
sturniolo <br1.sturniolo@gmail.com>
2022-12-12 16:21:23 -0300

    [ADD]cliente en catalog

--- api-catalog/src/main/java/com/dh/catalog/api/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/api/client/MovieServiceClient.java
@@ -1,10 +1,14 @@
 package com.dh.catalog.api.client;
 
+import com.dh.catalog.domain.models.Movie;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -13,6 +17,9 @@ public interface MovieServiceClient {
 
 	@GetMapping("movies/{genre}")
 	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
+
+	@PostMapping("/salvar")
+	ResponseEntity<Movie> saveMovie(@RequestBody Movie movie);
 
 
 	@Getter
