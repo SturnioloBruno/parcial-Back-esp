189b7421bda7960e52d0c228a50724f01d0e2a92
sturniolo <br1.sturniolo@gmail.com>
2022-12-12 20:04:15 -0300

    [FIX]api-series

--- api-serie/src/main/java/com/dh/apiserie/ApiSerieApplication.java
+++ api-serie/src/main/java/com/dh/apiserie/ApiSerieApplication.java
@@ -4,6 +4,7 @@ import com.dh.apiserie.model.Chapter;
 import com.dh.apiserie.model.Season;
 import com.dh.apiserie.model.Series;
 import com.dh.apiserie.repository.SerieRepositoryMongo;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import java.util.List;
 
 @SpringBootApplication
+@EnableRabbit
 @EnableMongoRepositories(basePackages = "com.dh.apiserie.repository")
 public class ApiSerieApplication {
 
@@ -20,33 +22,6 @@ public class ApiSerieApplication {
 		SpringApplication.run(ApiSerieApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData(SerieRepositoryMongo repository) {
-		return (args) -> {
-			if (!repository.findAll().isEmpty()) {
-				return;
-			}
-			Chapter chapter1Season1 = new Chapter(null, "Primero", 1, "www.netflix.com");
-			Chapter chapter2Season1 = new Chapter(null, "Segundo", 2, "www.netflix.com");
-			Chapter chapter1Season2 = new Chapter(null, "Tercero", 1, "www.netflix.com");
-			Chapter chapter2Season2 = new Chapter(null, "Cuarto", 2, "www.netflix.com");
-			List<Chapter> chapterList = null;
-			chapterList.add(chapter1Season1);
-			chapterList.add(chapter2Season1);
-			Season season1 = new Season(null, 1, chapterList);
-			chapterList.remove(0);
-			chapterList.remove(1);
-			chapterList.add(chapter1Season2);
-			chapterList.add(chapter2Season2);
-			Season season2 = new Season(null, 2, chapterList);
-			List<Season> seasonList = null;
-			seasonList.add(season1);
-			seasonList.add(season2);
-			repository.save(new Series(null, "Serie 1", "Terror", seasonList));
-			repository.save(new Series(null, "Serie 2", "Terror", seasonList));
-			repository.save(new Series(null, "Serie 3", "Comedia", seasonList));
-			repository.save(new Series(null, "Serie 4", "Fantasia", seasonList));
-		};
-	}
+
 
 }
